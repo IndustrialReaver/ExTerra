82824d5c186d0d95d8e597daf916a340f42a1b72
Ben <industrialreaver@gmail.com>
2015-09-28 16:25:33 -0400

    Added main menu

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        Application.LoadLevel("ExTerraStaging");
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void About()
+    {
+
+    }
+}
