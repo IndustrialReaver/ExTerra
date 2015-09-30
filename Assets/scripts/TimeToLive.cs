6244562fe4a1ea0b09739d6c05ac18e6f7e394bf
Ben <industrialreaver@gmail.com>
2015-09-30 16:15:18 -0400

    added basic logo to main menu, tweaked player movement

---
+++ Assets/scripts/TimeToLive.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeToLive : MonoBehaviour {
+
+    public int FramesToLive = 20;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FramesToLive--;
+        if (FramesToLive <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
--- Assets/scripts/bulletMove.cs
+++ Assets/scripts/bulletMove.cs
@@ -35,6 +35,8 @@ public class bulletMove : MonoBehaviour {
         if (coll.transform != parent)
         {
             coll.gameObject.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+            GameObject ping = Instantiate(Resources.Load("SheildPing"), transform.position, transform.rotation) as GameObject;
+            ping.transform.parent = coll.gameObject.transform;
             GameObject gui = Instantiate(Resources.Load("DamageText"), Camera.main.WorldToViewportPoint(transform.position), Quaternion.identity) as GameObject;
             gui.GetComponent<GUIText>().text = damage.ToString();
 
