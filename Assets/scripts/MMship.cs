5d21fdb3d68c731130400010d85e9ebe4be8f6e4
Ben <industrialreaver@gmail.com>
2015-10-16 17:17:22 -0400

    Continued general changes, updated main menu

---
+++ Assets/scripts/MMship.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MMship : MonoBehaviour {
+
+    public float speed = 0.01f;
+
+    private bool screenoffset = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        Vector2 pos = transform.position;
+
+        if ((!this.GetComponent<Renderer>().isVisible) && screenoffset)
+        {
+            pos.x *= -1;
+            transform.position = pos;
+            screenoffset = false;
+        }
+
+        if (this.GetComponent<Renderer>().isVisible)
+        {
+            screenoffset = true;
+        }
+
+	}
+}
